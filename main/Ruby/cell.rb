d7e9b4c90197d9ad5a0b5ce187256521e0c720ff
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-05-28 18:05:08 -0700

    Rewrote cell class to make sure columns were a class variable.

---
+++ main/Ruby/cell.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Cell
+  # The column headers of cells.csv as attributes of the Cell class
+  @@columns = [:oem, :model, :launch_announced, :launch_status, :body_dimensions,
+               :body_weight, :body_sim, :display_type, :display_size, :display_resolution,
+               :features_sensors, :platform_os]
+  attr_accessor *@@columns
+
+  def initialize(oem, model, launch_announced, launch_status, body_dimensions,
+                 body_weight, body_sim, display_type, display_size, display_resolution,
+                 features_sensors, platform_os)
+    @oem = oem
+    @model = model
+    @launch_announced = launch_announced
+    @launch_status = launch_status
+    @body_dimensions = body_dimensions
+    @body_weight = body_weight
+    @body_sim = body_sim
+    @display_type = display_type
+    @display_size = display_size
+    @display_resolution = display_resolution
+    @features_sensors = features_sensors
+    @platform_os = platform_os
+  end
+
+  def toString()
+    # code goes here
+  end
+
+  def delete()
+    # code goes here
+  end
+end
+
+
