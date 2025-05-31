ba7efec51bc576c6f2ddc58203e3dfd7625a4408
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-05-30 21:04:44 -0700

    Put some regex into def initialize to make sure that some variables are either in the correct format or are nil

--- main/Ruby/cell.rb
+++ main/Ruby/cell.rb
@@ -4,7 +4,11 @@ require 'csv'
 class Cell
   # Stored in a hash class attribute
 
-  @@cell_data = Hash.new
+  @@cell_data = {}
+
+  def self.cell_data
+    @@cell_data
+  end
 
   # The column headers of cells.csv as attributes of the Cell class
   @oem
@@ -19,50 +23,70 @@ class Cell
   @display_resolution
   @features_sensors
   @platform_os
-  attr_accessor @oem,@model,@launch_announced,@launch_status,@body_dimensions,@body_sim,@display_type,@display_size,
-                @display_resolution,@features_sensors,@platform_os
+  attr_accessor :oem,:model,:launch_announced,:launch_status,:body_dimensions,:body_sim,:display_type,:display_size,
+                :display_resolution,:features_sensors,:platform_os
 
+  # Constructor, using regex to match strings in valid formats; instances with other strings will have
+  # nil for that value.
+  # Fields that accept any string as valid, thus do not need regex: oem, model, body_dimensions, display_type,
+  # and display_resolution.
   def initialize(oem, model, launch_announced, launch_status, body_dimensions,
                  body_weight, body_sim, display_type, display_size, display_resolution,
                  features_sensors, platform_os)
     @oem = oem
     @model = model
-    @launch_announced = launch_announced
-    @launch_status = launch_status
+    @launch_announced = launch_announced[/\b\d{4}\b/]&.to_i # Captures first occurrence of 4-digit year, if any.
+    @launch_status = launch_status[/(?:Discontinued|Cancelled|(Available.\. Released \d{4}))/]
+    # Captures "Discontinued", "Cancelled", and "Available. Released <4-digit year>", ignores later characters.
     @body_dimensions = body_dimensions
-    @body_weight = body_weight
-    @body_sim = body_sim
+    @body_weight = body_weight[/\A\d+(?:\.\d+)?(?=[ g(])/]
+    # Captures ints, floats, ignores following white space,'g', and '(' characters
+    @body_sim = body_sim[/\A(?!Yes\z|No\z).+/]# Allows all strings except "Yes" and "No"
     @display_type = display_type
-    @display_size = display_size
+    @display_size = display_size[/\A\d+(?:\.\d+)?(?=[ i])/] # Any integer or float followed by a space and the 'i' in inches
     @display_resolution = display_resolution
-    @features_sensors = features_sensors
-    @platform_os = platform_os
+    @features_sensors = features_sensors[/(?=.*[A-Za-z]).+/]# Allows all strings that contain at least one letter
+    @platform_os = platform_os[/(?=.*[A-Za-z]).+/]# Allows all strings that contain at least one letter
     
-    @@cell_data.store(self, @oem)
+    @@cell_data[self] = @oem
     
   end
 
   def toString()
     puts "OEM: #{@oem}, Model: #{@model}, Launch_Announced: #{@launch_announced}, Launch_Status: #{@launch_status},
 body_dimensions: #{@body_dimensions}, body_weight: #{@body_weight}, body_sim: #{@body_sim}, display_type: #{@display_type},
-display_size: #{display_size}, display_resolution: #{display_resolution}, features_sensors: #{@features_sensors},
+display_size: #{@display_size}, display_resolution: #{@display_resolution}, features_sensors: #{@features_sensors},
 platform_os: #{@platform_os}"
   end
 
   # Class methods to find the average of Launch_Announced, body_weight, and display_size:
-  def Cell.find_average_launch
-    avg_launch =
-    puts "Average year that launch was announced: #{avg_launch}"
+
+  # Puts the keys (Cell objects, each called a phone here) into an array if @launch_announced is not nill (.compact),
+  # then checks if the array is not empty and divides the sum of the keys in float form by the size of the array.
+  def Cell.find_average_launch_announced
+    keys = @@cell_data.keys.map{|phone| phone.launch_announced}.compact
+    if keys.any?
+      avg_launch = keys.sum.to_f / keys.size
+      puts "Average year that launch was announced: #{avg_launch}"
+    end
   end
 
+  # Puts the keys (Cell objects, each called a phone here) into an array if @body_weight is not nill (.compact),
+  # then checks if the array is not empty and divides the sum of the keys in float form by the size of the array.
   def Cell.find_average_weight
-    avg_weight =
-    puts "Average weight: #{avg_weight}"
+    keys = @@cell_data.keys.map{|phone| phone.body_weight}.compact
+    if keys.any?
+      avg_weight = keys.sum.to_f / keys.size
+      puts "Average weight: #{avg_weight}"
+    end
   end
 
   def Cell.find_average_display_size
-    avg_display_size =
-    puts "Average display size: #{avg_display_size}"
+    keys = @@cell_data.keys.map{|phone| phone.display_size}.compact
+    if keys.any?
+      avg_display_size = keys.sum.to_f / keys.size
+      puts "Average display size: #{avg_display_size}"
+    end
   end
 
 
